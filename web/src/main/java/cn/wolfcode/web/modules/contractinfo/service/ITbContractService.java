2b5fa3842cfc349d29a722ead142ead1c856ed74
Milk-Ice <1946872913@qq.com>
2023-06-29 16:07:08 +0800

    生成器生成合同管理模块

---
+++ web/src/main/java/cn/wolfcode/web/modules/contractinfo/service/ITbContractService.java
@@ -0,0 +1,16 @@
+package cn.wolfcode.web.modules.contractinfo.service;
+
+import cn.wolfcode.web.modules.contractinfo.entity.TbContract;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 合同信息 服务类
+ * </p>
+ *
+ * @author hmy
+ * @since 2023-06-29
+ */
+public interface ITbContractService extends IService<TbContract> {
+
+}
